6139d74287bebe02a714f7016802f98b4d613547
Daniela Dottolo <ddottolo.mmt-b2018@fh-salzburg.ac.at>
2021-01-13 11:31:11 +0100

    Update ruby version, implement action cable, rubocop, prettier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,38 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("jquery");
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import chatRoomChannel from "../channels/chat_room_channel";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+$(document).on("turbolinks:load", function () {
+  $("form#set_name").on("submit", function (e) {
+    e.preventDefault();
+    let name = $("#add_name").val();
+    sessionStorage.setItem("chat_room_name", name);
+    chatRoomChannel.announce({ name, type: "join" });
+    $("#modal").css("display", "none");
+  });
+
+  $("form#send_message").on("submit", function (e) {
+    e.preventDefault();
+    let message = $("#message").val();
+    if (message.length > 0) {
+      chatRoomChannel.speak(message);
+      $("#message").val("");
+    }
+  });
+
+  $(window).on("beforeunload", function () {
+    let name = sessionStorage.getItem("chat_room_name");
+    chatRoomChannel.announce({ name, type: "leave" });
+  });
+});
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
-require "active_support/core_ext/integer/time"
+# frozen_string_literal: true
+
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -35,6 +37,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_caching = false
 
