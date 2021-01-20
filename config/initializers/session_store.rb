8fdb809bfb43a4e67541bab1045602d7bd4a5189
nillmichaela <michaela.schoesswendter@nill.at>
2021-01-20 22:41:03 +0100

    edit session store for development

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://fierce-springs-89185.herokuapp.com"
+if Rails.env === "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://fierce-springs-89185.herokuapp.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
