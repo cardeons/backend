59855032b0188e94b8662364e021ba211c44488c
nillmichaela <michaela.schoesswendter@nill.at>
2021-02-01 12:19:02 +0100

    return user if logged_in true

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < ActionController::Base
     
       def authorized
         render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
-        user_frontend = {"id": @current_user.id,"email": @current_user.email,"created_at": @current_user.created_at,"updated_at": @current_user.updated_at,"name": @current_user.name}
+        user_frontend = {"id": @current_user.id,"email": @current_user.email,"name": @current_user.name}
         render json: { user: user_frontend } unless !logged_in?
       
       end
