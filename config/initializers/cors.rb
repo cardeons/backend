70dbb69ea14cda7c144503bd9c0a5522fa9fd668
Daniela Dottolo <ddottolo.mmt-b2018@fh-salzburg.ac.at>
2021-02-01 15:34:30 +0100

    Allow all origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "http://localhost:*"
-        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+        origins "*"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: false
     end
 
     #frontend domain hier :)
