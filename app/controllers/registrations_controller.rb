9e5367855e7774a5ab23f7435c54ceaa5245628e
nillmichaela <michaela.schoesswendter@nill.at>
2021-02-01 12:31:06 +0100

    add json error messages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < ApplicationController
     def create
         registration_input = request.raw_post()
         registration_input = JSON.parse(registration_input)
-        user = User.create!(
+        user = User.new(
             email: registration_input['email'],
             name: registration_input['name'],
             password: registration_input['password'],
@@ -10,14 +10,14 @@ class RegistrationsController < ApplicationController
         )
         user_frontent = {"id": user.id,"email": user.email,"created_at": user.created_at,"updated_at": user.updated_at,"name": user.name}
 
-        if user
+        if user.save
             session[:user_id] = user.id
             render json: {
                 status: :created,
                 user: user_frontent
             }
         else
-            render json: { status: 400 }
+            render :json => { :errors => user.errors.as_json }, :status => 420
         end
     end
 end
