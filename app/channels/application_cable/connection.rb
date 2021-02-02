fb5bed890bb0413f4866e3ed6f3a45b81bd6dc1a
Christoph <cmayerhofer.mmt-b2018@fh-salzburg.ac.at>
2021-02-02 15:10:28 +0100

    added socket with authorization

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,5 +2,49 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+      puts "-----------------------------"
+      puts current_user.email
+      puts "found current User"
+    end
+
+    private
+
+    def auth_header
+      # { Authorization: 'Bearer <token>' }
+      request.headers['Authorization']
+    end
+
+  
+    def decoded_token
+      if auth_header
+        token = auth_header.split[1]
+        # header: { 'Authorization': 'Bearer <token>' }
+        begin
+          JWT.decode(token, 's3cr3t', true, algorithm: 'HS256')
+        rescue JWT::DecodeError
+          nil
+        end
+      end
+    end
+  
+    def logged_in_user
+      if decoded_token
+        user_id = decoded_token[0]['user_id']
+        User.find_by(id: user_id)
+      end
+    end
+  
+
+    def find_verified_user
+      if logged_in_user
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
