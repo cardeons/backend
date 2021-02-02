fe33c224bdba199bc83575f4a2c6d569d804494a
Christoph Mayerhofer <christoph.julian.mayerhofer@gmail.com>
2021-02-02 19:29:53 +0100

    players now subscribe to gamechannel and player

--- db/seeds.rb
+++ db/seeds.rb
@@ -101,9 +101,9 @@ Buffcard.create(id: 9,
 Gameboard.create(id: 1, current_state: 'fight', player_atk: 5)
 
 # Player.create(id: 1, name: 'Gustav', gameboard_id: 1, user_id: 1)
-Player.create(id: 2, name: 'Thomas', gameboard_id: 1, user_id: 2)
-Player.create(id: 3, name: 'Lorenz', gameboard_id: 1, user_id: 3)
-Player.create(id: 4, name: 'Maja', gameboard_id: 1, user_id: 4)
+# Player.create(id: 2, name: 'Thomas', gameboard_id: 1, user_id: 2)
+# Player.create(id: 3, name: 'Lorenz', gameboard_id: 1, user_id: 3)
+# Player.create(id: 4, name: 'Maja', gameboard_id: 1, user_id: 4)
 
 Handcard.create(id: 1, player_id: 1)
 Ingamedeck.create(id: 1,  gameboard_id: 1, card_id: 1, cardable_id: 1, cardable_type: 'Handcard')
