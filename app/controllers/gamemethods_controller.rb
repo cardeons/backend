287cd176400eeccaaef7a8ec59ae3826de170777
nillmichaela <michaela.schoesswendter@nill.at>
2021-02-02 14:59:59 +0100

    add lint

--- app/controllers/gamemethods_controller.rb
+++ app/controllers/gamemethods_controller.rb
@@ -1,154 +1,155 @@
+# frozen_string_literal: true
 
 class GamemethodsController < ApplicationController
-    def draw_doorcard
-        cursecards = Cursecard.all
-        monstercards = Monstercard.all
-        bosscards = Bosscard.all
+  def draw_doorcard
+    cursecards = Cursecard.all
+    monstercards = Monstercard.all
+    bosscards = Bosscard.all
 
-        allcards = []
+    allcards = []
 
-        for card in cursecards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card
-                x -=1
-            end
-        end
-
-        for card in monstercards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card
-                x -=1
-            end
-        end
-
-        for card in bosscards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card
-                x -=1
-            end
-        end
-
-        randomcard = allcards[rand(allcards.length)]
-
-        render json: { card: randomcard }, status: 200
+    cursecards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card
+        x -= 1
+      end
     end
 
-    def draw_treasurecard
-        cursecards = Cursecard.all
-        monstercards = Monstercard.all
-        buffcards = Buffcard.all
-        itemcards = Itemcard.all
-        levelcards = Levelcard.all
-
-        allcards = []
-
-        for card in cursecards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card
-                x -=1
-            end
-        end
-
-        for card in monstercards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card
-                x -=1
-            end
-        end
-
-        for card in buffcards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card
-                x -=1
-            end
-        end
-
-        for card in itemcards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card
-                x -=1
-            end
-        end
-
-        for card in levelcards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card
-                x -=1
-            end
-        end
-
-        randomcard = allcards[rand(allcards.length)]
-
-        render json: { card: randomcard }, status: 200
+    monstercards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card
+        x -= 1
+      end
     end
 
-    def draw_handcards
-        cursecards = Cursecard.all
-        monstercards = Monstercard.all
-        buffcards = Buffcard.all
-        itemcards = Itemcard.all
-        levelcards = Levelcard.all
-
-        allcards = []
-
-        for card in cursecards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card.id
-                x -=1
-            end
-        end
-
-        for card in monstercards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card.id
-                x -=1
-            end
-        end
-
-        for card in buffcards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card.id
-                x -=1
-            end
-        end
-
-        for card in itemcards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card.id
-                x -=1
-            end
-        end
-
-        for card in levelcards do
-            x = card.draw_chance
-            while x > 0
-                allcards.push card.id
-                x -=1
-            end
-        end
-
-        handcard = Player.find(params[:id]).handcard
-
-        Ingamedeck.where(cardable_type: 'Handcard', cardable_id: handcard.id).delete_all
-
-        Ingamedeck.create(gameboard_id: params[:gameboard_id], card_id: allcards[rand(allcards.length)], cardable_id: handcard.id, cardable_type: 'Handcard')
-        Ingamedeck.create(gameboard_id: params[:gameboard_id], card_id: allcards[rand(allcards.length)], cardable_id: handcard.id, cardable_type: 'Handcard')
-        Ingamedeck.create(gameboard_id: params[:gameboard_id], card_id: allcards[rand(allcards.length)], cardable_id: handcard.id, cardable_type: 'Handcard')
-        Ingamedeck.create(gameboard_id: params[:gameboard_id], card_id: allcards[rand(allcards.length)], cardable_id: handcard.id, cardable_type: 'Handcard')
-        Ingamedeck.create(gameboard_id: params[:gameboard_id], card_id: allcards[rand(allcards.length)], cardable_id: handcard.id, cardable_type: 'Handcard')
-        
-        render json: { card: handcard.cards }, status: 200
+    bosscards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card
+        x -= 1
+      end
     end
+
+    randomcard = allcards[rand(allcards.length)]
+
+    render json: { card: randomcard }, status: 200
+  end
+
+  def draw_treasurecard
+    cursecards = Cursecard.all
+    monstercards = Monstercard.all
+    buffcards = Buffcard.all
+    itemcards = Itemcard.all
+    levelcards = Levelcard.all
+
+    allcards = []
+
+    cursecards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card
+        x -= 1
+      end
+    end
+
+    monstercards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card
+        x -= 1
+      end
+    end
+
+    buffcards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card
+        x -= 1
+      end
+    end
+
+    itemcards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card
+        x -= 1
+      end
+    end
+
+    levelcards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card
+        x -= 1
+      end
+    end
+
+    randomcard = allcards[rand(allcards.length)]
+
+    render json: { card: randomcard }, status: 200
+  end
+
+  def draw_handcards
+    cursecards = Cursecard.all
+    monstercards = Monstercard.all
+    buffcards = Buffcard.all
+    itemcards = Itemcard.all
+    levelcards = Levelcard.all
+
+    allcards = []
+
+    cursecards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card.id
+        x -= 1
+      end
+    end
+
+    monstercards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card.id
+        x -= 1
+      end
+    end
+
+    buffcards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card.id
+        x -= 1
+      end
+    end
+
+    itemcards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card.id
+        x -= 1
+      end
+    end
+
+    levelcards.each do |card|
+      x = card.draw_chance
+      while x.positive?
+        allcards.push card.id
+        x -= 1
+      end
+    end
+
+    handcard = Player.find(params[:id]).handcard
+
+    Ingamedeck.where(cardable_type: 'Handcard', cardable_id: handcard.id).delete_all
+
+    Ingamedeck.create(gameboard_id: params[:gameboard_id], card_id: allcards[rand(allcards.length)], cardable_id: handcard.id, cardable_type: 'Handcard')
+    Ingamedeck.create(gameboard_id: params[:gameboard_id], card_id: allcards[rand(allcards.length)], cardable_id: handcard.id, cardable_type: 'Handcard')
+    Ingamedeck.create(gameboard_id: params[:gameboard_id], card_id: allcards[rand(allcards.length)], cardable_id: handcard.id, cardable_type: 'Handcard')
+    Ingamedeck.create(gameboard_id: params[:gameboard_id], card_id: allcards[rand(allcards.length)], cardable_id: handcard.id, cardable_type: 'Handcard')
+    Ingamedeck.create(gameboard_id: params[:gameboard_id], card_id: allcards[rand(allcards.length)], cardable_id: handcard.id, cardable_type: 'Handcard')
+
+    render json: { card: handcard.cards }, status: 200
+  end
 end
