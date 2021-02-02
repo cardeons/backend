cd7f443593ed3ecfe87d4cbeb050355b13bd96b2
Christoph Mayerhofer <christoph.julian.mayerhofer@gmail.com>
2021-02-02 19:47:08 +0100

    made connection more clear

--- app/channels/gamesocket_channel.rb
+++ app/channels/gamesocket_channel.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class GamesocketChannel < ApplicationCable::Channel
-  rescue_from 'MyError', with: :deliver_error_message
+  rescue_from Exception, with: :deliver_error_message
 
   def subscribed
     awaiting_players = 'awaiting_players'
@@ -38,19 +38,19 @@ class GamesocketChannel < ApplicationCable::Channel
     @gameboard = Gameboard.find(gameboard.id)
     stream_for @gameboard
 
-    broadcast_to(@gameboard, "new Player conected to the gameboard id: #{@gameboard.id}")
+    broadcast_to(@gameboard, "new Player#{current_user.email} conected to the gameboard id: #{@gameboard.id}")
     broadcast_to(@gameboard, "players in lobby: #{@gameboard.players.count}")
 
 
-    PlayerChannel.broadcast_to( @gameboard.players.first.user, "only one should get this this user should be: #{@gameboard.players.first.user} " )
+   @gameboard.players.each do |player|
+    PlayerChannel.broadcast_to( player.user , "only you should get this you are  #{player.user.email}" )
+   end
 
 
     # if lobby is full tell other players
     if lobbyisfull
 
         players = @gameboard.players
-
-
         # much to do
         players.each do |player|
           handcard = Handcard.create(player_id: player.id)
@@ -64,25 +64,25 @@ class GamesocketChannel < ApplicationCable::Channel
 
           # broadcast_to(@gameboard, "player.handcard.cards")
 
-  
           # broadcast_to(@gameboard, player.handcard.cards)
           
           PlayerChannel.broadcast_to(player.user, player.handcard.cards )
         end
 
-
-
-
-
-
       broadcast_to(@gameboard, 'Lobby is full start with game')
-      broadcast_to(@gameboard, { action: 'init', gameboard: @gameboard.players })
+      broadcast_to(@gameboard, { action: 'startGame', gameboard: @gameboard.players })
     end
   end
 
   def play_card(data)
     # ##add actions!
+
+    dataObject = JSON.parse data
     puts data
+
+
+
+    
     broadcast_to(@gameboard, { action: 'player played', data: data })
   end
 
@@ -93,6 +93,6 @@ class GamesocketChannel < ApplicationCable::Channel
   private
 
   def deliver_error_message(e)
-    # broadcast_to(...)
+    broadcast_to(...)
   end
 end
