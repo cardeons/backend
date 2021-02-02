a50e93a048898de6043e0d891bb1bfb715626c7f
Daniela Dottolo <ddottolo.mmt-b2018@fh-salzburg.ac.at>
2021-02-02 14:53:59 +0100

    Add user to player

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_140204) do
+ActiveRecord::Schema.define(version: 2021_02_02_122552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,7 +137,9 @@ ActiveRecord::Schema.define(version: 2021_02_01_140204) do
     t.bigint "gameboard_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["gameboard_id"], name: "index_players_on_gameboard_id"
+    t.index ["user_id"], name: "index_players_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -161,4 +163,5 @@ ActiveRecord::Schema.define(version: 2021_02_01_140204) do
   add_foreign_key "monstertwos", "players"
   add_foreign_key "playercurses", "players"
   add_foreign_key "players", "gameboards"
+  add_foreign_key "players", "users"
 end
