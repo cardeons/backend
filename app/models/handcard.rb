f1c842d9c7a81246e0810ea60589020ee7d96ac9
nillmichaela <michaela.schoesswendter@nill.at>
2021-02-02 14:58:29 +0100

    add draw methods

--- app/models/handcard.rb
+++ app/models/handcard.rb
@@ -3,5 +3,6 @@
 class Handcard < ApplicationRecord
   # belongs_to :ingamedeck
   has_many :ingamedecks, as: :cardable, dependent: :destroy
+  has_many :cards, through: :ingamedecks
   belongs_to :player
 end
