a3d5ef9a2557a0733bd15282b81ee9b4880b602a
Christoph Mayerhofer <christoph.julian.mayerhofer@gmail.com>
2021-02-02 18:11:12 +0100

    linting

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,119 +15,119 @@
 ActiveRecord::Schema.define(version: 2021_02_02_122552) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "cards", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "image"
-    t.string "action"
-    t.integer "draw_chance"
-    t.integer "level"
-    t.string "element"
-    t.string "bad_things"
-    t.string "rewards_treasure"
-    t.string "good_against"
-    t.string "bad_against"
-    t.integer "good_against_value"
-    t.integer "bad_against_value"
-    t.integer "element_modifier"
-    t.integer "atk_points"
-    t.string "item_category"
-    t.integer "has_combination"
-    t.integer "level_amount"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "type"
+  create_table 'cards', force: :cascade do |t|
+    t.string 'title'
+    t.string 'description'
+    t.string 'image'
+    t.string 'action'
+    t.integer 'draw_chance'
+    t.integer 'level'
+    t.string 'element'
+    t.string 'bad_things'
+    t.string 'rewards_treasure'
+    t.string 'good_against'
+    t.string 'bad_against'
+    t.integer 'good_against_value'
+    t.integer 'bad_against_value'
+    t.integer 'element_modifier'
+    t.integer 'atk_points'
+    t.string 'item_category'
+    t.integer 'has_combination'
+    t.integer 'level_amount'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'type'
   end
 
-  create_table "cards_users", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "card_id", null: false
+  create_table 'cards_users', id: false, force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'card_id', null: false
   end
 
-  create_table "gameboards", force: :cascade do |t|
-    t.string "current_state"
-    t.integer "player_atk"
-    t.integer "monster_atk"
-    t.boolean "asked_help"
-    t.boolean "success"
-    t.boolean "can_flee"
-    t.integer "shared_reward"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'gameboards', force: :cascade do |t|
+    t.string 'current_state'
+    t.integer 'player_atk'
+    t.integer 'monster_atk'
+    t.boolean 'asked_help'
+    t.boolean 'success'
+    t.boolean 'can_flee'
+    t.integer 'shared_reward'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "graveyards", force: :cascade do |t|
-    t.bigint "gameboard_id", null: false
-    t.bigint "ingamedeck_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["gameboard_id"], name: "index_graveyards_on_gameboard_id"
-    t.index ["ingamedeck_id"], name: "index_graveyards_on_ingamedeck_id"
+  create_table 'graveyards', force: :cascade do |t|
+    t.bigint 'gameboard_id', null: false
+    t.bigint 'ingamedeck_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['gameboard_id'], name: 'index_graveyards_on_gameboard_id'
+    t.index ['ingamedeck_id'], name: 'index_graveyards_on_ingamedeck_id'
   end
 
-  create_table "handcards", force: :cascade do |t|
-    t.bigint "player_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["player_id"], name: "index_handcards_on_player_id"
+  create_table 'handcards', force: :cascade do |t|
+    t.bigint 'player_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['player_id'], name: 'index_handcards_on_player_id'
   end
 
-  create_table "ingamedecks", force: :cascade do |t|
-    t.bigint "card_id", null: false
-    t.bigint "gameboard_id", null: false
-    t.string "cardable_type"
-    t.bigint "cardable_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["card_id"], name: "index_ingamedecks_on_card_id"
-    t.index ["cardable_type", "cardable_id"], name: "index_ingamedecks_on_cardable"
-    t.index ["gameboard_id"], name: "index_ingamedecks_on_gameboard_id"
+  create_table 'ingamedecks', force: :cascade do |t|
+    t.bigint 'card_id', null: false
+    t.bigint 'gameboard_id', null: false
+    t.string 'cardable_type'
+    t.bigint 'cardable_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['card_id'], name: 'index_ingamedecks_on_card_id'
+    t.index %w[cardable_type cardable_id], name: 'index_ingamedecks_on_cardable'
+    t.index ['gameboard_id'], name: 'index_ingamedecks_on_gameboard_id'
   end
 
-  create_table "interceptcards", force: :cascade do |t|
-    t.bigint "gameboard_id", null: false
-    t.bigint "ingamedeck_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["gameboard_id"], name: "index_interceptcards_on_gameboard_id"
-    t.index ["ingamedeck_id"], name: "index_interceptcards_on_ingamedeck_id"
+  create_table 'interceptcards', force: :cascade do |t|
+    t.bigint 'gameboard_id', null: false
+    t.bigint 'ingamedeck_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['gameboard_id'], name: 'index_interceptcards_on_gameboard_id'
+    t.index ['ingamedeck_id'], name: 'index_interceptcards_on_ingamedeck_id'
   end
 
-  create_table "inventories", force: :cascade do |t|
-    t.bigint "player_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["player_id"], name: "index_inventories_on_player_id"
+  create_table 'inventories', force: :cascade do |t|
+    t.bigint 'player_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['player_id'], name: 'index_inventories_on_player_id'
   end
 
-  create_table "monsterones", force: :cascade do |t|
-    t.bigint "player_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["player_id"], name: "index_monsterones_on_player_id"
+  create_table 'monsterones', force: :cascade do |t|
+    t.bigint 'player_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['player_id'], name: 'index_monsterones_on_player_id'
   end
 
-  create_table "monsterthrees", force: :cascade do |t|
-    t.bigint "player_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["player_id"], name: "index_monsterthrees_on_player_id"
+  create_table 'monsterthrees', force: :cascade do |t|
+    t.bigint 'player_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['player_id'], name: 'index_monsterthrees_on_player_id'
   end
 
-  create_table "monstertwos", force: :cascade do |t|
-    t.bigint "player_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["player_id"], name: "index_monstertwos_on_player_id"
+  create_table 'monstertwos', force: :cascade do |t|
+    t.bigint 'player_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['player_id'], name: 'index_monstertwos_on_player_id'
   end
 
-  create_table "playercurses", force: :cascade do |t|
-    t.bigint "player_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["player_id"], name: "index_playercurses_on_player_id"
+  create_table 'playercurses', force: :cascade do |t|
+    t.bigint 'player_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['player_id'], name: 'index_playercurses_on_player_id'
   end
 
   create_table "players", force: :cascade do |t|
@@ -142,12 +144,12 @@ ActiveRecord::Schema.define(version: 2021_02_02_122552) do
     t.index ["user_id"], name: "index_players_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+  create_table 'users', force: :cascade do |t|
+    t.string 'email'
+    t.string 'password_digest'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'name'
   end
 
   add_foreign_key "graveyards", "gameboards"
