80eb1635a88dfb78cda553d4233bff3b2bfc6ea7
Christoph Mayerhofer <christoph.julian.mayerhofer@gmail.com>
2021-02-03 22:03:25 +0100

    fixed a bug in gameboard

--- app/models/gameboard.rb
+++ app/models/gameboard.rb
@@ -19,9 +19,7 @@ class Gameboard < ApplicationRecord
       unless player.handcard
         Handcard.create(player_id: player.id)
       end
-
       Handcard.draw_handcards(player.id, gameboard)
-
     end
 
   end
@@ -29,26 +27,52 @@ class Gameboard < ApplicationRecord
   def self.broadcast_game_board(gameboard)
     players_array = []
 
+    gameboard = Gameboard.find(gameboard.id)
+
+    Centercard.create(gameboard_id: gameboard.id)
+
     gameboard.players.each do |player|
       # ##only for debug
       # TODO: remove later
-      Inventory.create(player_id: player.id) unless player.inventory
+      Inventory.create(player: player) unless player.inventory
 
-      Handcard.create(player_id: player.id) unless player.handcard
+      pp player.inventory.cards
 
-      Monsterone.create(player_id: player.id) unless player.monsterone
+      Handcard.create(player: player) unless player.handcard
 
-      Monstertwo.create(player_id: player.id) unless player.monstertwo
+      pp player.handcard.cards
 
-      Monsterthree.create(player_id: player.id) unless player.monsterthree
+      Monsterone.create(player: player) unless player.monsterone
+
+      pp player.monsterone.cards
+
+      Monstertwo.create(player: player) unless player.monstertwo
+
+      pp player.monstertwo.cards
+      Monsterthree.create(player: player) unless player.monsterthree
+
+      pp player.monsterthree.cards
+
+      Playercurse.create(player: player) unless player.playercurse
+
+      pp player.playercurse.cards
+      pp "player ud"
+      pp player.id
 
       players_array.push({ name: player.name, player_id: player.id, inventory: player.inventory.cards, handcard: player.handcard.cards.count, monsterone: player.monsterone.cards, monstertwo: player.monstertwo.cards,
                           monsterthree: player.monsterthree.cards })
     end
 
+    pp     output = { # add center
+      # graveyard: gameboard.graveyard,
+      players: players_array,
+      # needs more info
+      gameboard: gameboard
+    }
+
     output = { # add center
       # graveyard: gameboard.graveyard,
-      players: playersArray,
+      players: players_array,
       # needs more info
       gameboard: gameboard
     }
