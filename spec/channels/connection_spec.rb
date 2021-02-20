10fe34abf64709312ed124ac98d38cf32898453b
Christoph Mayerhofer <christoph.julian.mayerhofer@gmail.com>
2021-02-20 12:19:02 +0100

    Added Tests for connection to /cable

---
+++ spec/channels/connection_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationCable::Connection, type: :channel do
+  fixtures :users
+
+  it 'successfully connects with valid JWT' do
+    connect '/cable', headers: { 'HTTP_SEC_WEBSOCKET_PROTOCOL' => JWT.encode({ user_id: 1 }, 's3cr3t') }
+    expect(connection.current_user.id).to eq 1
+  end
+
+  
+  it 'rejects connection without valid JWT' do
+    expect { connect '/cable', headers: { 'HTTP_SEC_WEBSOCKET_PROTOCOL' => '1' } }.to have_rejected_connection
+  end
+
+  it 'rejects connection without JWT' do
+    expect { connect '/cable' }.to have_rejected_connection
+  end
+end
