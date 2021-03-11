38c9652a454b2a918d852bb23aa5d33d15587f91
Schoesswendter <34650722+Schoesswendter@users.noreply.github.com>
2021-03-11 11:33:54 +0100

    Update game_channel.rb

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -67,8 +67,7 @@ class GameChannel < ApplicationCable::Channel
 
   def equip_monster(params)
     player = Player.find_by('user_id = ?', current_user.id)
-    puts 'PARAMS LOOK LIKE THIS'
-    puts params
+
     result = Monstercard.equip_monster(params, player)
 
     updated_board = Gameboard.broadcast_game_board(@gameboard)
