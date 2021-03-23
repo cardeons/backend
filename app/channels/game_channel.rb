a97a367e54b1c32ed17390d476df878db2c3e443
dottolodaniela <56515804+dottolodaniela@users.noreply.github.com>
2021-03-23 15:26:44 +0100

    Update game_channel.rb

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -195,41 +195,6 @@ class GameChannel < ApplicationCable::Channel
     end
   end
 
-  # def play_card(params)
-  #   # add actions!
-
-  #   paramsObject = JSON.parse params
-  #   puts paramsObject
-
-  #   broadcast_to(@gameboard, { type: DEBUG, params: { message: 'You just used play_card with ', params: paramsObject } })
-
-  #   case paramsObject.to
-  #   when 'Inventory'
-  #     broadcast_to(@gameboard, { type: DEBUG, params: { message: "Player #{current_user.email} just played to inventory" } })
-  #     current_card = Ingamedeck.find_by('id=?', paramsObject.unique_id)
-  #     current_card.update_attribute(:cardable_type, 'Inventory')
-  #   when 'Monsterone'
-  #     broadcast_to(@gameboard, { type: DEBUG, params: { message: "Player #{current_user.email} just played to monsterone" } })
-  #     current_card = Ingamedeck.find_by('id=?', paramsObject.unique_id)
-  #     current_card.update_attribute(:cardable_type, 'Monsterone')
-  #   when 'Monstertwo'
-  #     broadcast_to(@gameboard, { type: DEBUG, params: { message: "Player #{current_user.email} just played to monstertwo" } })
-  #     current_card = Ingamedeck.find_by('id=?', paramsObject.unique_id)
-  #     current_card.update_attribute(:cardable_type, 'Monstertwo')
-  #   when 'Monsterthree'
-  #     broadcast_to(@gameboard, { type: DEBUG, params: { message: "Player #{current_user.email} just played to monsterthree" } })
-  #     current_card = Ingamedeck.find_by('id=?', paramsObject.unique_id)
-  #     current_card.update_attribute(:cardable_type, 'Monsterthree')
-  #   when 'center'
-  #     broadcast_to(@gameboard, { type: DEBUG, params: { message: "Player #{current_user.email} just played to center" } })
-  #     # TODO: currently not implemented
-  #   else
-  #     broadcast_to(@gameboard, { type: ERROR, params: { message: "Player #{current_user.email} just played to something i dont know" } })
-  #   end
-
-  #   broadcast_to(@gameboard, { type: BOARD_UPDATE, params: Gameboard.broadcast_game_board(@gameboard) })
-  # end
-
   def help_call(params)
     helping_player = Player.find_by('id = ?', params['helping_player_id'])
     helping_shared_reward = params['helping_shared_rewards']
