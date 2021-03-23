2f84fb04da94839ead15f7885c47fd8ebbaf4920
Daniela Dottolo <ddottolo.mmt-b2018@fh-salzburg.ac.at>
2021-03-23 17:06:35 +0100

    equip monster now allows two hand items, add intercept value to player broadcast

--- app/models/monstercard.rb
+++ app/models/monstercard.rb
@@ -21,6 +21,10 @@ class Monstercard < Card
     #     #find "original" card, only advance if found
     unless deck_card.nil?
       card = Card.find_by('id=?', deck_card.card_id)
+
+      pp "---------------"
+      pp card
+      pp "---------------"
       # TODO: validieren
       cardtype = card.type
       #       # there already are 5 items, you can't put any more (6 because the monster itself is in this table)
@@ -31,8 +35,21 @@ class Monstercard < Card
 
       # category already on monster
       elsif monster_to_equip.cards.where('item_category=?', card.item_category).count.positive?
+
+        pp "in here"
+        pp card.item_category
+        pp monster_to_equip.cards.where('item_category=?', card.item_category).count
+
+        if card.item_category == "hand" && monster_to_equip.cards.where('item_category=?', card.item_category).count == 1
+          pp "erlaubt"
+          type = 'GAMEBOARD_UPDATE'
+          message = 'Successfully equipped.'
+          deck_card.update_attribute(:cardable, monster_to_equip)
+        else
+          pp "nicht erlaubt"
         type = 'ERROR'
         message = "You already have this type of item on your monster! (#{card.item_category})"
+        end
       # GameChannel.broadcast_to(gameboard, {type: 'ERROR', params: { message: "You already have this type of item on your monster! (#{card.item_category})" } })
 
       # not an item
@@ -44,6 +61,7 @@ class Monstercard < Card
       # yay
       else
 
+        pp "all good"
         type = 'GAMEBOARD_UPDATE'
         deck_card.update_attribute(:cardable, monster_to_equip)
 
