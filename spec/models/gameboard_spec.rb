8db66a9b132eeb5288490c5a2323a86f08cdabc4
Schoesswendter <m.schoesswendter@gmx.at>
2021-04-15 14:40:21 +0200

    adjust test

--- spec/models/gameboard_spec.rb
+++ spec/models/gameboard_spec.rb
@@ -106,6 +106,9 @@ RSpec.describe Gameboard, type: :model do
       can_flee: gameboards(:gameboardFourPlayers).can_flee,
       rewards_treasure: gameboards(:gameboardFourPlayers).rewards_treasure,
       graveyard: [],
+      helping_player_atk: gameboards(:gameboardFourPlayers).helping_player_atk,
+      shared_reward: gameboards(:gameboardFourPlayers).shared_reward,
+      helping_player: gameboards(:gameboardFourPlayers).helping_player,
       current_state: 'ingame',
       intercept_timestamp: nil
     }
