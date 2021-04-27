d7a1d793ce94076d651e4340b29975794f9822d0
Daniela Dottolo <ddottolo.mmt-b2018@fh-salzburg.ac.at>
2021-04-27 12:43:26 +0200

    Remove unneccessary code from test

--- spec/controllers/registration_spec.rb
+++ spec/controllers/registration_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe RegistrationsController < ActionController::TestCase do
 
   it 'register successfully' do
     json = { email: 'paul.bauer@gmail.com', name: 'xXSniperXx', password: 'blumen123', password_confirmation: 'blumen123' }.to_json
-    request.env['RAW_POST_DATA'] = json
     post :create, body: json
 
     expect(User.find_by('name=?', 'xXSniperXx')).to be_truthy
@@ -17,7 +16,6 @@ RSpec.describe RegistrationsController < ActionController::TestCase do
 
   it 'get card on register' do
     json = { email: 'paul.bauer@gmail.com', name: 'xXSniperXx', password: 'blumen123', password_confirmation: 'blumen123' }.to_json
-    request.env['RAW_POST_DATA'] = json
     post :create, body: json
 
     expect(User.find_by('name=?', 'xXSniperXx').cards.length).to eql(1)
