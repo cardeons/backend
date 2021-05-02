15037e140cf801488e2d4331afaa8908b44f743e
Schoesswendter <m.schoesswendter@gmx.at>
2021-05-02 17:53:15 +0200

    add online status to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_192108) do
+ActiveRecord::Schema.define(version: 2021_05_02_133200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -174,6 +174,7 @@ ActiveRecord::Schema.define(version: 2021_05_01_192108) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.boolean "online", default: false
   end
 
   add_foreign_key "centercards", "gameboards"
