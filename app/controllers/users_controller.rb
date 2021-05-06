8d49feaf05e1d6aecfd0fabb64d8bb5e7fd247dc
Schoesswendter <m.schoesswendter@gmx.at>
2021-05-06 10:13:28 +0200

    add authoriźation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,36 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    # @users = User.all
+  end
+
+  def decoded_token(token)
+    raise JWT::DecodeError 'ENV[ENC_KEY] is not set' unless ENV['ENC_KEY']
+
+    JWT.decode(token, ENV['ENC_KEY'], true, algorithm: 'HS256')
+  rescue JWT::DecodeError
+    nil
+  end
+
+  def find_verified_user
+    token = request.headers['token']
+
+    pp token
+    decoded_token = decoded_token(token)
+
+    # wrong type of JWT
+    return false unless decoded_token
+
+    if (current_user = User.find(decoded_token[0]['user_id']))
+      current_user
+    else
+      return false
+    end
+    true
   end
 
   def search
-    @users = User.where('name like ?', "%#{params[:search]}%")
+    @users = User.where('name like ?', "%#{params[:search]}%") if find_verified_user
   end
 
   # GET /users/1
